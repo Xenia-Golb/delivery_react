279f98cc2f2e822e441c2959518b97221777625d
Xenia Golbraykh <karzerin@gmail.com>
2024-07-09 19:20:51 +0300

    create product page

--- diplom-my-delivery/src/data/products.js
+++ diplom-my-delivery/src/data/products.js
@@ -1,30 +1,32 @@
 export const products = [
     {
         id: 1, title: 'Pizza Italy', description: 'Italian Pizza with tomato sauce, mozzarella, and basil',
-        rating: 4.5, price: 2.99, image: '/pizza.svg'
+        rating: 4.5, price: 2.99, image: '/pizza.svg', ingredients: ['tomato sauce', 'mozzarella', 'basil']
     },
     {
-        id: 2, title: 'Сaesar roll', description: 'This roll is a type of spring roll that consists of a thin layer of dough wrapped around a filling of fresh vegetables and chicken, seasoned with Caesar dressing. ', rating: 4.8, price: 3.99, image: '/roll.svg'
+        id: 2, title: 'Сaesar roll', description: 'This roll is a type of spring roll that consists of a thin layer of dough wrapped around a filling of fresh vegetables and chicken, seasoned with Caesar dressing. ', rating: 4.8, price: 3.99, image: '/roll.svg', ingredients: ['chicken breast fillet', 'caesar salad dressing', 'parmesan cheese', 'mixed salad leaves ']
     },
 
     {
         id: 3, title: 'Hot dog ', description: 'This is a popular type of fast food that consists of a long sausage called a "wurst" placed inside a bun.',
-        rating: 4.8, price: 3.49, image: '/hot-dog.svg'
+        rating: 4.8, price: 3.49, image: '/hot-dog.svg', ingredients: ['sausage', 'bun', 'ketchup', 'mustard']
     },
     {
-        id: 4, title: 'Pancakes', description: 'These are round, thick pancakes that are made from flour, milk, eggs and sugar. ', rating: 4.6, price: 2.99, image: '/pancakes.svg'
+        id: 4, title: 'Pancakes', description: 'These are round, thick pancakes that are made from flour, milk, eggs and sugar. ', rating: 4.6, price: 2.99, image: '/pancakes.svg', ingredients: [' milk', 'white vinegar', 'eggs', 'vanilla extract']
     },
     {
-        id: 5, title: 'Asian noodles', description: 'It can be made from various types of grains, including rice, wheat, buckwheat and other types of flour.', rating: 4.9, price: 3.99, image: '/asian-noodles.svg'
+        id: 5, title: 'Asian noodles', description: 'It can be made from various types of grains, including rice, wheat, buckwheat and other types of flour.', rating: 4.9, price: 3.99, image: '/asian-noodles.svg', ingredients: ['noodles', 'vegetables', 'beef']
     }
     , {
-        id: 6, title: 'Burger', description: 'This burger is a perfect combination of juicy meat patty, crispy lettuce, ripe tomatoes, creamy cheese, and aromatic onions, all sandwiched between two soft and fluffy buns.  ', rating: 4.7, price: 4.99, image: '/burger.svg'
+        id: 6, title: 'Burger', description: 'This burger is a perfect combination of juicy meat patty, crispy lettuce, ripe tomatoes, creamy cheese, and aromatic onions, all sandwiched between two soft and fluffy buns.  ', rating: 4.7, price: 4.99, image: '/burger.svg', ingredients: ['bun', 'beef', 'patty ', ' cheese ', ' tomato ', 'pickle']
     },
     {
-        id: 7, title: 'Steak', description: 'This steak is seasoned with a blend of herbs and spices, giving it a delicate flavor that complements the other ingredients perfectly. ', rating: 4.5, price: 5.99, image: '/steak.svg'
+        id: 7, title: 'Steak', description: 'This steak is seasoned with a blend of herbs and spices, giving it a delicate flavor that complements the other ingredients perfectly. ', rating: 4.5, price: 5.99, image: '/steak.svg',
+        ingredients: ['beef', 'salt', 'pepper', 'garlic', 'onion', 'herbs']
     },
 
     {
-        id: 8, title: 'Tomato soup', description: 'This soup prepared with a variety of spices, herbs and vegetables such as onions, garlic, carrots, celery, basil and thyme. ', rating: 4.2, price: 1.99, image: '/soup.svg'
+        id: 8, title: 'Tomato soup', description: 'This soup prepared with a variety of spices, herbs and vegetables such as onions, garlic, carrots, celery, basil and thyme. ', rating: 4.2, price: 1.99, image: '/soup.svg',
+        ingredients: ['tomato', 'onion', 'garlic', 'carrot', 'celery', 'basil', 'thyme']
     }
 ];
--- diplom-my-delivery/src/pages/Product/Product.jsx
+++ diplom-my-delivery/src/pages/Product/Product.jsx
@@ -1,11 +1,54 @@
+import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import { products } from '../../data/products';
+import Headling from '../../components/Headling/Headling';
+import Button from '../../components/Button/Button';
+import styles from './Product.module.css';
+import ProductCard from '../../components/ProductCard/ProductCard';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../../reducers/cart.slice';
 
 function Product() {
     const { id } = useParams();
+    const product = products.find((item) => item.id === parseInt(id));
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-    return (<div>
-        Product - {id}
-    </div>);
+    const add = (e) => {
+        e.preventDefault();
+        dispatch(addItem(product));
+        navigate('/cart');
+    };
+
+    return (<>
+        <div className={styles['header']} >
+            <button onClick={() => navigate(-1)} className={styles['back-btn']} ><img src="/left.svg" alt="back" /></button>
+            <Headling>{product.title}</Headling>
+            <Button onClick={add} appearance='primary'> <img src="/product-cart.svg" alt="cart" />&nbsp; Add to cart</Button></div>
+        <div className={styles['product']}>
+            <img className={styles['image']} src={product.image} alt={product.title} />
+            <div className={styles['info']}>
+                <div>{product.description}</div>
+                <div >Price:<span className={styles['price']}>{product.price}&nbsp;
+                    <span className={styles['currency']}>$</span></span></div>
+                <div>Rating:<span className={styles['rating']}>{product.rating}&nbsp;
+                    <img src="/star-icon.svg" alt="rating" /></span></div>
+                <div className={styles['ingredients']}>Ingredients:{product.ingredients.map((i, index) => {
+                    return <li key={index}>{i}</li>;
+                })}
+                </div>
+            </div>
+        </div>
+        <div className={styles['recommendations']}>
+            <h3 className={styles['title']}>Related products</h3>
+            <div className={styles['list']}>
+                {products.filter((item) => item.id !== product.id).slice(0, 3).map((item) => (
+                    <ProductCard key={item.id} {...item} />
+                ))}
+            </div>
+        </div>
+
+    </>);
 }
 
 export default Product;
