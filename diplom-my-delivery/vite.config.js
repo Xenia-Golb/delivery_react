0d716ce1b9f7842ff26560b99442e14e710527c9
Xenia Golbraykh <karzerin@gmail.com>
2024-07-13 18:37:47 +0300

    build

--- diplom-my-delivery/vite.config.js
+++ diplom-my-delivery/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-delivery/',
+  base: '/diplom-my-delivery//',
   plugins: [react()]
 });
